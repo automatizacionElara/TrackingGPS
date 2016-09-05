c6596df290d6d769640cc4abce78f113fc097f3c
Daniel Sosa <daniel.sosa@elara.com.mx>
2016-09-05 15:31:52 -0500

    GPS

---
+++ app/src/main/java/elaracomunicaciones/gpstracking/RegisterService.java
@@ -0,0 +1,64 @@
+package elaracomunicaciones.gpstracking;
+
+import android.os.AsyncTask;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by daniel sosa on 05/09/2016.
+ */
+public class RegisterService extends AsyncTask<Void, Void, Boolean> {
+
+    private final int IdTechnician;
+    private final int IdService;
+    private boolean IsSuccess;
+    String msg = "";
+
+    RegisterService(int IdTech, int IdServ) {
+        IdTechnician = IdTech;
+        IdService = IdServ;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        try {
+            String msg = "";
+            Connection con = DBConnection.getInstance().getConnection();
+            if (con == null) {
+                msg = "Error en la Conexión con SQL server";
+            } else {
+                String Elara_ES_TrackingDetails = "INSERT INTO Elara_ES_Tracking VALUES(1," +  IdTechnician + "," + IdService + ");";
+                Statement stmt = null;
+
+                try {
+                    stmt = con.createStatement();
+                    stmt.executeQuery(Elara_ES_TrackingDetails);
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception ex) {
+            IsSuccess = false;
+            msg = "Exceptions";
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+        super.onPostExecute(success);
+
+    }
+
+    @Override
+    protected void onCancelled() {
+
+    }
+
+}
+
+
