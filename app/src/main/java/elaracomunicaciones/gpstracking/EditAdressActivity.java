e0b3dfd96bebd1ce6b74cd0121c08e4f37870ced
Daniel Sosa <daniel.sosa@elara.com.mx>
2016-10-28 10:59:08 -0500

    28 OCTUBRE 2016
    - Nuevo diseño

---
+++ app/src/main/java/elaracomunicaciones/gpstracking/EditAdressActivity.java
@@ -0,0 +1,13 @@
+package elaracomunicaciones.gpstracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAdressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editadress);
+    }
+}
