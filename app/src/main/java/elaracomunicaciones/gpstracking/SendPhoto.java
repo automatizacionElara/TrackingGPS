baf0e42045f6a162d0bd727ad06346190ef90269
luis aranda <luis.aranda@elara.com.mx>
2016-11-14 15:22:05 -0600

    Ultimo Cambio

---
+++ app/src/main/java/elaracomunicaciones/gpstracking/SendPhoto.java
@@ -0,0 +1,79 @@
+package elaracomunicaciones.gpstracking;
+
+import android.os.AsyncTask;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by daniel sosa on 29/08/2016.
+ */
+
+    public class SendPhoto extends AsyncTask<Void, Void, Boolean> {
+
+        private final String stringPhoto;
+        private final int IdService;
+        private final String Fecha;
+        private boolean IsSuccess;
+        String msg = "";
+
+        SendPhoto(int IdServ, String photo, String fecha) {
+            IdService = IdServ;
+            stringPhoto = photo;
+            Fecha = "";
+        }
+
+    /*SendPhoto(int IdServ, double Long, double Lat, String fecha)
+    {
+         latitude = Lat;
+         longitude = Long;
+        IdService = IdServ;
+         Fecha = fecha;
+    }*/
+
+
+
+    @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                String Elara_ES_TrackingDetails = "";
+                String msg = "";
+                Connection con = DBConnection.getInstance().getConnection();
+                if (con == null) {
+                    msg = "Error en la Conexión con SQL server";
+                } else
+                {
+
+                        Elara_ES_TrackingDetails = "INSERT INTO Elara_ES_Photos VALUES(" + IdService + ","+ Fecha + "," + stringPhoto + ");";
+
+                    Statement stmt = null;
+
+                    try {
+                        stmt = con.createStatement();
+                        stmt.executeQuery(Elara_ES_TrackingDetails);
+                        stmt.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception ex) {
+                IsSuccess = false;
+                msg = "Exceptions";
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            super.onPostExecute(success);
+        }
+
+        @Override
+        protected void onCancelled() {
+
+        }
+
+    }
+
